6b57912cd7d0f4866db593f2b738750dcaedb397
julytian <992113471@qq.com>
2017-07-04 22:12:08 +0800

    add postcss-loader in units.js

--- build/utils.js
+++ build/utils.js
@@ -1,74 +1,76 @@
 var path = require('path')
 var config = require('../config')
-// 提取css样式到一个共同文件
+    // 提取css样式到一个共同文件
 var ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 // 静态资源路径
-exports.assetsPath = function (_path) {
-  var assetsSubDirectory = process.env.NODE_ENV === 'production'
-    ? config.build.assetsSubDirectory
-    : config.dev.assetsSubDirectory
-  return path.posix.join(assetsSubDirectory, _path)
+exports.assetsPath = function(_path) {
+    var assetsSubDirectory = process.env.NODE_ENV === 'production' ?
+        config.build.assetsSubDirectory :
+        config.dev.assetsSubDirectory
+    return path.posix.join(assetsSubDirectory, _path)
 }
 
 // css loaders
-exports.cssLoaders = function (options) {
-  options = options || {}
+exports.cssLoaders = function(options) {
+    options = options || {}
 
-  var cssLoader = {
-    loader: 'css-loader',
-    options: {
-      minimize: process.env.NODE_ENV === 'production',
-      sourceMap: options.sourceMap
+    var cssLoader = {
+        loader: 'css-loader',
+        options: {
+            minimize: process.env.NODE_ENV === 'production',
+            sourceMap: options.sourceMap
+        }
     }
-  }
+    var postcssLoader = {
+            loader: 'postcss-loader'
+        }
+        // generate loader string to be used with extract text plugin
+    function generateLoaders(loader, loaderOptions) {
+        var loaders = [cssLoader, postcssLoader]
+        if (loader) {
+            loaders.push({
+                loader: loader + '-loader',
+                options: Object.assign({}, loaderOptions, {
+                    sourceMap: options.sourceMap
+                })
+            })
+        }
 
-  // generate loader string to be used with extract text plugin
-  function generateLoaders (loader, loaderOptions) {
-    var loaders = [cssLoader]
-    if (loader) {
-      loaders.push({
-        loader: loader + '-loader',
-        options: Object.assign({}, loaderOptions, {
-          sourceMap: options.sourceMap
-        })
-      })
+        // Extract CSS when that option is specified
+        // (which is the case during production build)
+        // 如果是vuejs则使用 fallback: 'vue-style-loader'
+        if (options.extract) {
+            return ExtractTextPlugin.extract({
+                use: loaders,
+                fallback: 'style-loader'
+            })
+        } else {
+            return ['style-loader'].concat(loaders)
+        }
     }
 
-    // Extract CSS when that option is specified
-    // (which is the case during production build)
-    // 如果是vuejs则使用 fallback: 'vue-style-loader'
-    if (options.extract) {
-      return ExtractTextPlugin.extract({
-        use: loaders,
-        fallback: 'style-loader'
-      })
-    } else {
-      return ['style-loader'].concat(loaders)
+    return {
+        css: generateLoaders(),
+        postcss: generateLoaders(),
+        less: generateLoaders('less'),
+        sass: generateLoaders('sass', { indentedSyntax: true }),
+        scss: generateLoaders('sass'),
+        stylus: generateLoaders('stylus'),
+        styl: generateLoaders('stylus')
     }
-  }
-
-  return {
-    css: generateLoaders(),
-    postcss: generateLoaders(),
-    less: generateLoaders('less'),
-    sass: generateLoaders('sass', { indentedSyntax: true }),
-    scss: generateLoaders('sass'),
-    stylus: generateLoaders('stylus'),
-    styl: generateLoaders('stylus')
-  }
 }
 
 // Generate loaders for standalone style files (outside of .vue)
-exports.styleLoaders = function (options) {
-  var output = []
-  var loaders = exports.cssLoaders(options)
-  for (var extension in loaders) {
-    var loader = loaders[extension]
-    output.push({
-      test: new RegExp('\\.' + extension + '$'),
-      use: loader
-    })
-  }
-  return output
+exports.styleLoaders = function(options) {
+    var output = []
+    var loaders = exports.cssLoaders(options)
+    for (var extension in loaders) {
+        var loader = loaders[extension]
+        output.push({
+            test: new RegExp('\\.' + extension + '$'),
+            use: loader
+        })
+    }
+    return output
 }
