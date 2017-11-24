b5e7025d840664a66f097f0136644f71c3dff6eb
julytian <chenwenyi@youcheyihou.com>
2017-11-24 16:05:44 +0800

    升级webpack3.0和修改配置

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -1,68 +1,73 @@
-var path = require('path')
-var utils = require('./utils')
-var config = require('../config')
+var path = require('path');
+var utils = require('./utils');
+var config = require('../config');
 
 function resolve(dir) {
-    return path.join(__dirname, '..', dir)
+	return path.join(__dirname, '..', dir);
 }
 
 module.exports = {
-    entry: {
-        app: './src/main.js'
-    },
-    output: {
-        path: config.build.assetsRoot,
-        filename: '[name].js',
-        publicPath: process.env.NODE_ENV === 'production'
-            ? config.build.assetsPublicPath
-            : config.dev.assetsPublicPath
-    },
-    resolve: {
-        extensions: ['.js', '.vue', '.json'],
-        alias: {
-            'vue$': 'vue/dist/vue.esm.js',
-            'src': resolve('src'),
-            'images': resolve('src/assets/images'),
-            'sass': resolve('src/assets/sass'),
-            'fonts': resolve('src/assets/fonts')
-        }
-    },
-    module: {
-        rules: [{ // 对src和test文件夹下的.js文件使用babel-loader
-            test: /\.js$/,
-            loader: 'babel-loader',
-            include: [resolve('src'), resolve('test')]
-        },
-        { // 对图片资源文件使用url-loader，query.name指明了输出的命名规则
-            test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
-            loader: 'url-loader',
-            options: {
-                limit: 10000,
-                name: utils.assetsPath('images/[name].[hash:7].[ext]')
-            }
-        },
-        {
-            test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
-            loader: 'url-loader',
-            options: {
-                limit: 10000,
-                name: utils.assetsPath('media/[name].[hash:7].[ext]')
-            }
-        },
-        {
-            test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
-            loader: 'url-loader',
-            options: {
-                limit: 10000,
-                name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
-            }
-        },
-        {
-            test: /\.html$/,
-            use: [{
-                loader: 'html-loader?attrs[]=img:src&attrs[]=img:data-src'
-            }]
-        }
-        ]
-    }
-}
+	context: path.resolve(__dirname, '../'),
+	entry: {
+		app: './src/main.js'
+	},
+	output: {
+		path: config.build.assetsRoot,
+		filename: '[name].js',
+		publicPath: process.env.NODE_ENV === 'production' ? config.build.assetsPublicPath : config.dev.assetsPublicPath
+	},
+	resolve: {
+		extensions: [ '.js', '.vue', '.json' ],
+		alias: {
+			vue$: 'vue/dist/vue.esm.js',
+			'@': resolve('src'),
+			src: resolve('src'),
+			images: resolve('src/assets/images'),
+			sass: resolve('src/assets/sass'),
+			fonts: resolve('src/assets/fonts')
+		}
+	},
+	module: {
+		rules: [
+			{
+				// 对src和test文件夹下的.js文件使用babel-loader
+				test: /\.js$/,
+				loader: 'babel-loader',
+				include: [ resolve('src'), resolve('test') ]
+			},
+			{
+				// 对图片资源文件使用url-loader，query.name指明了输出的命名规则
+				test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+				loader: 'url-loader',
+				options: {
+					limit: 10000,
+					name: utils.assetsPath('images/[name].[hash:7].[ext]')
+				}
+			},
+			{
+				test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
+				loader: 'url-loader',
+				options: {
+					limit: 10000,
+					name: utils.assetsPath('media/[name].[hash:7].[ext]')
+				}
+			},
+			{
+				test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+				loader: 'url-loader',
+				options: {
+					limit: 10000,
+					name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
+				}
+			},
+			{
+				test: /\.html$/,
+				use: [
+					{
+						loader: 'html-loader?attrs[]=img:src&attrs[]=img:data-src'
+					}
+				]
+			}
+		]
+	}
+};
