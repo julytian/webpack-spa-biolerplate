6577611c9878056bfd3d25a087887390409a0c28
julytian <992113471@qq.com>
2017-12-29 23:09:58 +0800

    modify uglifyjs

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -3,71 +3,81 @@ var utils = require('./utils');
 var config = require('../config');
 
 function resolve(dir) {
-	return path.join(__dirname, '..', dir);
+    return path.join(__dirname, '..', dir);
 }
 
 module.exports = {
-	context: path.resolve(__dirname, '../'),
-	entry: {
-		app: './src/main.js'
-	},
-	output: {
-		path: config.build.assetsRoot,
-		filename: '[name].js',
-		publicPath: process.env.NODE_ENV === 'production' ? config.build.assetsPublicPath : config.dev.assetsPublicPath
-	},
-	resolve: {
-		extensions: [ '.js', '.vue', '.json' ],
-		alias: {
-			vue$: 'vue/dist/vue.esm.js',
-			'@': resolve('src'),
-			src: resolve('src'),
-			images: resolve('src/assets/images'),
-			sass: resolve('src/assets/sass'),
-			fonts: resolve('src/assets/fonts')
-		}
-	},
-	module: {
-		rules: [
-			{
-				// 对src和test文件夹下的.js文件使用babel-loader
-				test: /\.js$/,
-				loader: 'babel-loader',
-				include: [ resolve('src'), resolve('test') ]
-			},
-			{
-				// 对图片资源文件使用url-loader，query.name指明了输出的命名规则
-				test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
-				loader: 'url-loader',
-				options: {
-					limit: 10000,
-					name: utils.assetsPath('images/[name].[hash:7].[ext]')
-				}
-			},
-			{
-				test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
-				loader: 'url-loader',
-				options: {
-					limit: 10000,
-					name: utils.assetsPath('media/[name].[hash:7].[ext]')
-				}
-			},
-			{
-				test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
-				loader: 'url-loader',
-				options: {
-					limit: 10000,
-					name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
-				}
-			},
-			{
-				test: /\.html$/,
-				use: [
-					{
-						loader: 'html-loader?attrs[]=img:src&attrs[]=img:data-src'
-					}
-				]
-			}
-		]
-	}
+    context: path.resolve(__dirname, "../"),
+    entry: {
+        app: "./src/main.js"
+    },
+    output: {
+        path: config.build.assetsRoot,
+        filename: "[name].js",
+        publicPath: process.env.NODE_ENV === "production" ?
+            config.build.assetsPublicPath : config.dev.assetsPublicPath
+    },
+    resolve: {
+        extensions: [".js", ".vue", ".json"],
+        alias: {
+            vue$: "vue/dist/vue.esm.js",
+            "@": resolve("src"),
+            src: resolve("src"),
+            images: resolve("src/assets/images"),
+            sass: resolve("src/assets/sass"),
+            fonts: resolve("src/assets/fonts")
+        }
+    },
+    module: {
+        rules: [{
+                // 对src和test文件夹下的.js文件使用babel-loader
+                test: /\.js$/,
+                loader: "babel-loader",
+                include: [resolve("src"), resolve("test")]
+            },
+            {
+                // 对图片资源文件使用url-loader，query.name指明了输出的命名规则
+                test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+                loader: "url-loader",
+                options: {
+                    limit: 10000,
+                    name: utils.assetsPath("images/[name].[hash:7].[ext]")
+                }
+            },
+            {
+                test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
+                loader: "url-loader",
+                options: {
+                    limit: 10000,
+                    name: utils.assetsPath("media/[name].[hash:7].[ext]")
+                }
+            },
+            {
+                test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+                loader: "url-loader",
+                options: {
+                    limit: 10000,
+                    name: utils.assetsPath("fonts/[name].[hash:7].[ext]")
+                }
+            },
+            {
+                test: /\.html$/,
+                use: [{
+                    loader: "html-loader?attrs[]=img:src&attrs[]=img:data-src"
+                }]
+            }
+        ]
+    },
+    node: {
+        // prevent webpack from injecting useless setImmediate polyfill because Vue
+        // source contains it (although only uses it if it's native).
+        setImmediate: false,
+        // prevent webpack from injecting mocks to Node native modules
+        // that does not make sense for the client
+        dgram: "empty",
+        fs: "empty",
+        net: "empty",
+        tls: "empty",
+        child_process: "empty"
+    }
 };
--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -7,79 +7,75 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 var FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin');
 const portfinder = require('portfinder');
 
-// add hot-reload related code to entry chunks
-// Object.keys(baseWebpackConfig.entry).forEach(function(name) {
-//     baseWebpackConfig.entry[name] = ['./build/dev-client'].concat(baseWebpackConfig.entry[name])
-// })
+const HOST = process.env.HOST;
+const PORT = process.env.PORT && Number(process.env.PORT);
 
-const devWebpackConfig =  merge(baseWebpackConfig, {
-	// 配置样式文件的处理规则，使用styleLoaders
-	module: {
-		rules: utils.styleLoaders({ sourceMap: config.dev.cssSourceMap, usePostCSS: true })
-	},
-	// cheap-module-eval-source-map is faster for development
-	devtool: config.dev.devtool,
-	// these devServer options should be customized in /config/index.js
-	devServer: {
-		clientLogLevel: 'warning',
-		historyApiFallback: true,
-		hot: true,
-		compress: true,
-		host: process.env.HOST || config.dev.host,
-		port: process.env.PORT || config.dev.port,
-		open: config.dev.autoOpenBrowser,
-		overlay: config.dev.errorOverlay
-			? {
-					warnings: false,
-					errors: true
-				}
-			: false,
-		publicPath: config.dev.assetsPublicPath,
-		proxy: config.dev.proxyTable,
-		quiet: true, // necessary for FriendlyErrorsPlugin
-		watchOptions: {
-			poll: config.dev.poll
-		}
-	},
-	plugins: [
-		new webpack.DefinePlugin({
-			'process.env': require('../config/dev.env')
-		}),
-		// https://github.com/glenjamin/webpack-hot-middleware#installation--usage
-		new webpack.HotModuleReplacementPlugin(),
-		new webpack.NamedModulesPlugin(), // HMR shows correct file names in console on update.
-		new webpack.NoEmitOnErrorsPlugin(),
-		// https://github.com/ampedandwired/html-webpack-plugin
-		new HtmlWebpackPlugin({
-			filename: 'index.html',
-			template: 'index.html',
-			inject: true
-		})
-	]
+const devWebpackConfig = merge(baseWebpackConfig, {
+    // 配置样式文件的处理规则，使用styleLoaders
+    module: {
+        rules: utils.styleLoaders({ sourceMap: config.dev.cssSourceMap, usePostCSS: true })
+    },
+    // cheap-module-eval-source-map is faster for development
+    devtool: config.dev.devtool,
+    // these devServer options should be customized in /config/index.js
+    devServer: {
+        clientLogLevel: 'warning',
+        historyApiFallback: true,
+        hot: true,
+        compress: true,
+        host: HOST || config.dev.host,
+        port: PORT || config.dev.port,
+        open: config.dev.autoOpenBrowser,
+        overlay: config.dev.errorOverlay ? {
+            warnings: false,
+            errors: true
+        } : false,
+        publicPath: config.dev.assetsPublicPath,
+        proxy: config.dev.proxyTable,
+        quiet: true, // necessary for FriendlyErrorsPlugin
+        watchOptions: {
+            poll: config.dev.poll
+        }
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env': require('../config/dev.env')
+        }),
+        // https://github.com/glenjamin/webpack-hot-middleware#installation--usage
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NamedModulesPlugin(), // HMR shows correct file names in console on update.
+        new webpack.NoEmitOnErrorsPlugin(),
+        // https://github.com/ampedandwired/html-webpack-plugin
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: 'index.html',
+            inject: true
+        })
+    ]
 });
 
 module.exports = new Promise((resolve, reject) => {
-	portfinder.basePort = process.env.PORT || config.dev.port;
-	portfinder.getPort((err, port) => {
-		if (err) {
-			reject(err);
-		} else {
-			// publish the new Port, necessary for e2e tests
-			process.env.PORT = port;
-			// add port to devServer config
-			devWebpackConfig.devServer.port = port;
+    portfinder.basePort = process.env.PORT || config.dev.port;
+    portfinder.getPort((err, port) => {
+        if (err) {
+            reject(err);
+        } else {
+            // publish the new Port, necessary for e2e tests
+            process.env.PORT = port;
+            // add port to devServer config
+            devWebpackConfig.devServer.port = port;
 
-			// Add FriendlyErrorsPlugin
-			devWebpackConfig.plugins.push(
-				new FriendlyErrorsPlugin({
-					compilationSuccessInfo: {
-						messages: [ `Your application is running here: http://${config.dev.host}:${port}` ]
-					},
-					onErrors: config.dev.notifyOnErrors ? utils.createNotifierCallback() : undefined
-				})
-			);
+            // Add FriendlyErrorsPlugin
+            devWebpackConfig.plugins.push(
+                new FriendlyErrorsPlugin({
+                    compilationSuccessInfo: {
+                        messages: [`Your application is running here: http://${config.dev.host}:${port}`]
+                    },
+                    onErrors: config.dev.notifyOnErrors ? utils.createNotifierCallback() : undefined
+                })
+            );
 
-			resolve(devWebpackConfig);
-		}
-	});
+            resolve(devWebpackConfig);
+        }
+    });
 });
